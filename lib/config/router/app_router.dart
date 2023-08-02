b87728350ebcf37ca49fe68631fbdcbec7e22e7c
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-02 12:57:43 -0600

    add package go_router add file route

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/screen.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttoms',
+      builder: (context, state) => const ButtomScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const CardScreen(),
+    ),
+  ],
+);
