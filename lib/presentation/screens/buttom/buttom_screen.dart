be1a7ca315fa0abd06ffd3bbbcc3b799911c4907
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-02 13:22:20 -0600

    work on button

--- lib/presentation/screens/buttom/buttom_screen.dart
+++ lib/presentation/screens/buttom/buttom_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtomScreen extends StatelessWidget {
   const ButtomScreen({super.key});
@@ -6,12 +7,49 @@ class ButtomScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Buttom Screen'),
-      ),
-      body: const Center(
-        child: Text('Buttom Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Buttom Screen'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              tooltip: 'Open shopping cart',
+              onPressed: () {
+                // handle the press
+              },
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Text Button')),
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.access_alarm_outlined),
+                  label: const Text('Text Button Icon')),
+              FilledButton(
+                  onPressed: () {}, child: const Text('Filled Button')),
+              FilledButton.tonal(
+                  onPressed: () {}, child: const Text('Filled Button tonal')),
+              FilledButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.abc),
+                  label: const Text('Filled Buttom Icon')),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.abc)),
+              IconButton.filled(
+                  onPressed: () {}, icon: const Icon(Icons.ac_unit)),
+              IconButton.filledTonal(
+                  onPressed: () {}, icon: const Icon(Icons.abc_sharp))
+            ],
+          ),
+        ));
   }
 }
