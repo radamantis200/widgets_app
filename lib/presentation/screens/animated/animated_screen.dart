544b70421fe77fc4cb0333bae08ed1a82dc644bb
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-05 11:11:50 -0600

    add screens animated and ui_controls

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,52 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    borderRadius = random.nextInt(30) + 20;
+
+    color = Color.fromRGBO(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Animated stateful widget')),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeOutCubic,
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius), color: color),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => changeShape(),
+        child: const Icon(Icons.play_arrow_outlined),
+      ),
+    );
   }
 }
--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,112 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Control')),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = false;
+  Transportation selectedTransportation = Transportation.car;
+
+  void changeStatus(bool value) {
+    isDeveloper = value;
+    setState(() {});
+  }
+
+  void changeSelected(Transportation value) {
+    setState(() {
+      selectedTransportation = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionles'),
+          value: isDeveloper,
+          onChanged: (value) => changeStatus(value),
+        ),
+        ExpansionTile(
+          initiallyExpanded: true,
+          title: const Text('Vehículo de transporte'),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            CustomRadioListTitle(
+              value: Transportation.car,
+              groupValue: selectedTransportation,
+              title: 'By Car',
+              subTitle: 'Viajar por carro',
+              onChange: (value) => changeSelected(value!),
+            ),
+            CustomRadioListTitle(
+              value: Transportation.boat,
+              groupValue: selectedTransportation,
+              title: 'By Boat',
+              subTitle: 'Viajar por barco',
+              onChange: (value) => changeSelected(value!),
+            ),
+            CustomRadioListTitle(
+              value: Transportation.plane,
+              groupValue: selectedTransportation,
+              title: 'By Plane',
+              subTitle: 'Viajar por avión',
+              onChange: (value) => changeSelected(value!),
+            ),
+            CustomRadioListTitle(
+              value: Transportation.submarine,
+              groupValue: selectedTransportation,
+              title: 'By Submarine',
+              subTitle: 'Viajar por submarino',
+              onChange: (value) => changeSelected(value!),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CustomRadioListTitle extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final Transportation value;
+  final Transportation groupValue;
+  final void Function(Transportation?)? onChange;
+
+  const CustomRadioListTitle({
+    super.key,
+    required this.value,
+    required this.groupValue,
+    required this.title,
+    required this.subTitle,
+    required this.onChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RadioListTile(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      value: value,
+      groupValue: groupValue,
+      onChanged: onChange,
+    );
   }
 }
