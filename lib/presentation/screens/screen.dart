a6baf9bf901abff2b60f9327bc076fa9a2d4edd6
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-12 19:42:06 -0600

    add theme_changer_screen

--- lib/presentation/screens/screen.dart
+++ lib/presentation/screens/screen.dart
@@ -1,4 +1,3 @@
-
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
 export 'package:widgets_app/presentation/screens/card/card_screen.dart';
 export 'package:widgets_app/presentation/screens/buttom/buttom_screen.dart';
@@ -8,4 +7,5 @@ export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
 export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
 export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/theme_changer/theme_changer_screen.dart';
 export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
---
+++ lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
+
+class ThemeChangerScreen extends ConsumerWidget {
+  const ThemeChangerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isDarkMode = ref.watch(themeNotifierProvider).isDarkMode;
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme Changer'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(themeNotifierProvider.notifier).toogleDarkMode();
+              },
+              icon: Icon(isDarkMode
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined))
+        ],
+      ),
+      body: const _ThemeChangerView(),
+    );
+  }
+}
+
+class _ThemeChangerView extends ConsumerWidget {
+  const _ThemeChangerView();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Color> colors = ref.watch(colorListProvider);
+    final int selectedColor = ref.watch(themeNotifierProvider).seletectColor;
+    return ListView.builder(
+      itemCount: colors.length,
+      itemBuilder: (context, index) {
+        final Color color = colors[index];
+        return RadioListTile(
+          title: Text(
+            'Este color',
+            style: TextStyle(color: color),
+          ),
+          subtitle: Text('${color.value}'),
+          activeColor: color,
+          value: index,
+          groupValue: selectedColor,
+          onChanged: (value) {
+            ref.read(themeNotifierProvider.notifier).changeColorIndex(value!);
+          },
+        );
+      },
+    );
+  }
+}
