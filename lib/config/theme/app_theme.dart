1ae287af02324d4c8d1bc1a39209c3d5f89f3284
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-12 19:42:56 -0600

    change listColors add variable selectedColor add variable isDarkMode

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
-List<Color> colors = [
-  Colors.amber,
-  Colors.amberAccent,
-  Colors.black,
-  Colors.pink
+List<Color> listColors = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent
 ];
 
 class AppTheme {
   final int seletectColor;
-  AppTheme({this.seletectColor = 0})
+  final bool isDarkMode;
+  AppTheme({this.isDarkMode = false, this.seletectColor = 0})
       : assert(seletectColor >= 0, 'Selected Color must be greater then 0'),
-        assert(seletectColor < colors.length,
-            'Selected color mustt be less or equal then ${colors.length - 1}');
+        assert(seletectColor < listColors.length,
+            'Selected color mustt be less or equal then ${listColors.length - 1}');
   ThemeData getTheme() => ThemeData(
-      colorSchemeSeed: colors[seletectColor],
+      brightness: isDarkMode ? Brightness.dark : Brightness.light,
+      colorSchemeSeed: listColors[seletectColor],
       useMaterial3: true,
       appBarTheme: const AppBarTheme(centerTitle: true));
+
+  AppTheme copyWith({int? seletectColor, bool? isDarkMode}) {
+    return AppTheme(
+      seletectColor: seletectColor ?? this.seletectColor,
+      isDarkMode: isDarkMode ?? this.isDarkMode,
+    );
+  }
 }
